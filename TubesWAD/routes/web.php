c31953be4df7fc4e62a71539c58d822c6ae52877
febrianwulandari <febrian_wulandari_25rpl@student.smktelkom-mlg.sch.id>
2022-01-08 10:12:03 +0700

    commit regist

--- TubesWAD/routes/web.php
+++ TubesWAD/routes/web.php
@@ -1,12 +1,7 @@
 <?php
 
 use App\Http\Controllers\layanan;
-<<<<<<< HEAD
 use App\Http\Controllers\register;
-=======
-use App\Http\Controllers\pelanggan;
-use App\Models\Mpelanggan;
->>>>>>> b7d28edafff3bb2e44002702cb7363bb5b8c1384
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -45,12 +40,9 @@ Route::get('/layanan',[layanan::class, 'index']);
 Route::post('/layanan/add',[layanan::class, 'add']);
 Route::get('/layanan/viewadd',[layanan::class, 'viewAdd']);
 Route::get('/layanan/delete/{id}', [layanan::class, 'hapus']);
-<<<<<<< HEAD
 
 Route::get('/register',[register::class, 'index']);
 Route::post('/register/add',[register::class, 'store']);
-=======
 Route::post('/layanan/update',[layanan::class, 'update']);
 Route::get('/layanan/viewedit/{id}',[layanan::class, 'viewedit']);
 Route::get('/pelanggan',[pelanggan::class, 'index']);
->>>>>>> b7d28edafff3bb2e44002702cb7363bb5b8c1384
